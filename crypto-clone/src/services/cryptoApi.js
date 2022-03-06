26b0ca18303ed9ec33cb44957da47088841d4a73
DelhinRharl <delhinrharl@gmail.com>
2022-03-06 22:21:01 +0100

    Add api call using rtk query

---
+++ crypto-clone/src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query';
+
+const cryptoHeaders = {
+     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': '5b0af59a89msh874d44a75ee724ep18ecd8jsn2c3414ab8c44'
+}
+
+const baseUrl = "'https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) =>({url,headers:cryptoHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos:builder.query({
+            query:() => createRequest('/exchanges')
+        })
+    })
+})
