fbf5b6549316efcd895c24dcd538f4b4c0c2237e
DelhinRharl <delhinrharl@gmail.com>
2022-03-11 00:30:53 +0100

    Display data on homepage

--- crypto-clone/src/components/Homepage.js
+++ crypto-clone/src/components/Homepage.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Homepage = () => {
-const {data,isFetching} = useGetCryptosQuery();
+const {data, isFetching} = useGetCryptosQuery();
+const globalStats =data?.data?.stats;
     console.log(data);
+    if(isFetching) return  'Loading...'
   return (
-    <div>Homepage</div>
+    <div>
+     <div>Homepage</div> 
+     <div>Total Cryptocurrencies {globalStats.total} </div>
+     <div>Total Exchanges {globalStats.totalExchanges} </div>
+     <div>Total Market Cap {globalStats.totalMarketCap} </div>
+     <div>Total 24h Volume {globalStats.total24hVolume} </div>
+     <div>Total Markets {globalStats.totalMarkets} </div>
+    </div>
   )
 }
 
--- crypto-clone/src/services/cryptoApi.js
+++ crypto-clone/src/services/cryptoApi.js
@@ -14,7 +14,7 @@ export const cryptoApi = createApi({
     baseQuery:fetchBaseQuery({baseUrl}),
     endpoints: (builder) => ({
         getCryptos:builder.query({
-            query:() => createRequest('/coin/Qwsogvtv82FCd/exchanges')
+            query:() => createRequest('/coins')
         })
     })
 })
