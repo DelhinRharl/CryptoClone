6f1360da6cb2a890f9dda7da983ddb463963a82c
DelhinRharl <delhinrharl@gmail.com>
2022-07-07 14:58:45 +0100

    Edit homepage

--- crypto-clone/src/components/Homepage.js
+++ crypto-clone/src/components/Homepage.js
@@ -8,15 +8,16 @@ const globalStats =data?.data?.stats;
     console.log(data);
     if(isFetching) return  'Loading...'
   return (
-    <div className="relative left-40 w-full">
-     <div  className="bg-green-400" >Homepage</div> 
-     <div>Total Cryptocurrencies {globalStats.total} </div>
-     <div>Total Exchanges {globalStats.totalExchanges} </div>
-     <div>Total Market Cap {globalStats.totalMarketCap} </div>
-     <div>Total 24h Volume {globalStats.total24hVolume} </div>
-     <div>Total Markets {globalStats.totalMarkets} </div>
-
-     <div>
+    <div className=" flex-1 h-1/2">
+     <div  className="" >Homepage</div> 
+     <div className=" grid grid-cols-3 gap-2">
+     <div className="w-44 h-44 bg-slate-500 rounded-lg p-2 text-xl flex flex-col justify-center">Total Cryptocurrencies {globalStats.total} </div>
+     <div className="w-44 h-44 bg-slate-500 rounded-lg p-2 text-xl flex flex-col justify-center">Total Exchanges {globalStats.totalExchanges} </div>
+     <div className="w-44 h-44 bg-slate-500 rounded-lg p-2 text-xl flex flex-col justify-center">Total Market Cap {globalStats.totalMarketCap} </div>
+     <div className="w-44 h-44 bg-slate-500 rounded-lg p-2 text-xl flex flex-col justify-center">Total 24h Volume {globalStats.total24hVolume} </div>
+     <div className="w-44 h-44 bg-slate-500 rounded-lg p-2 text-xl flex flex-col justify-center">Total Markets {globalStats.totalMarkets} </div>
+    </div>
+     <div  className="text-5xl font-semibold">
        <h3>Top Ten CryptoCurrencies in the World</h3>
        <Cryptocurrencies/>
      </div>
