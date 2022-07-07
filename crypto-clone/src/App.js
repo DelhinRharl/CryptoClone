9143bb982d9352be9901660e875aa70f9c855510
DelhinRharl <delhinrharl@gmail.com>
2022-07-07 14:59:15 +0100

    Fix minor issues

--- crypto-clone/src/App.js
+++ crypto-clone/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Homepage from './components/Homepage';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
-  return (
-   <div className="flex">
+    return (
+   <div className="flex  bg-slate-200 gap-8">
+    <BrowserRouter>
      <Dashboard/>
      <Homepage/>
+    </BrowserRouter>
    </div>
-  )
+    );
 }
 
 export default App;
--- crypto-clone/tailwind.config.js
+++ crypto-clone/tailwind.config.js
@@ -2,8 +2,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  important : "#root",
   theme: {
     extend: {},
   },
+  variants: {
+    width: ["responsive", "hover", "focus", "active"],
+    height: ["responsive", "hover", "focus", "active"],
+  },
+
   plugins: [],
 }
