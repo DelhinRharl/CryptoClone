b4da2dae0066e56dfc098671d5cac3d09b225e23
DelhinRharl <delhinrharl@gmail.com>
2022-07-11 20:33:41 +0100

    Add routes to the dashboard elements

--- crypto-clone/src/App.js
+++ crypto-clone/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes,  Route } from 'react-router-dom';
 import Cryptocurrencies from './pages/Cryptocurrencies';
 import Dashboard from './components/Dashboard';
 import Homepage from './pages/Homepage';
@@ -7,9 +7,14 @@ function App() {
     return (
    <div className="flex bg-slate-200">
     <BrowserRouter>
-     <Dashboard/>
-     <Homepage/>
+    <Dashboard/>
+    <Routes>
+    <Route  path='/' element={<Homepage/>}/>
+    <Route  path='crypto' element={<Cryptocurrencies/>}/>
+    {/* <Route  path='news' element={<News/>}/> */}
+    </Routes>
     </BrowserRouter>
+
    </div>
     );
 }
--- crypto-clone/src/components/Dashboard.js
+++ crypto-clone/src/components/Dashboard.js
@@ -3,25 +3,33 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 import ArticleIcon from '@mui/icons-material/Article';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div className="w-1/4 text-gray-200  ">
         <aside className="w-1/6 h-full bg-black fixed left-0 top-0 rounded-tr-3xl  flex flex-col gap-8 items-center  p-2 text-lg ">
             <div className="w-full divide-y-[1px] divide-slate-700 pl-6 flex flex-col gap-8 text-2xl ">
+      <Link to="/">
         <div>
             <h1 className="text-4xl font-bold"> Cryptyn</h1>
-
         </div>
+        </Link>
+        <Link to="/">
         <div>
             <p className="flex items-center gap-4 hover:border-r-4 hover:border-indigo-700 hover:text-indigo-700">  <DashboardIcon className="text-indigo-700"/>Home</p>
         </div>
+        </Link>
+        <Link to="/crypto">
         <div>
             <p className="flex items-center gap-4 hover:border-r-4 hover:border-indigo-700 hover:text-indigo-700">  <CurrencyBitcoinIcon className="text-indigo-700"/>Cryptos</p>
         </div>
+        </Link>
+        <Link to="/news">
         <div>
             <p className="flex items-center gap-4 hover:border-r-4 hover:border-indigo-700 hover:text-indigo-700">  <ArticleIcon className="text-indigo-700"/>News</p>
         </div>
+        </Link>
         <Footer/>
         </div>
         </aside>
